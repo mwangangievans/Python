8aa123c1785f7d6f62e7b53653e31cd61f3ef783
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-02-27 05:17:35 +0300

    python variables

---
+++ Python DataTypes/variables/variables.py
@@ -0,0 +1,36 @@
+# creating variables...
+
+x = 5 
+y = "mwangangi" 
+print(x)
+print(y)
+
+# variables can change type in python dynamically
+
+k =  'mwangangi'
+k = 10 
+print(k)
+
+# Casting ===== If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x , y , z)
+
+# You can get the data type of a variable with the type() function.
+
+x = 100 
+y = "python"
+
+print(type(x))
+print(type(y))
+
+# Case-Sensitive === Variable names are case-sensitive.
+
+a = 100 
+A = "ken"
+
+#A will not overwrite a
+print(A) 
