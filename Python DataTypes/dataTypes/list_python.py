6a56bc8158b1801754fee3c8ab56577cbe24e9d8
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-02-28 15:49:35 +0300

    sorting list

--- Python DataTypes/dataTypes/list_python.py
+++ Python DataTypes/dataTypes/list_python.py
@@ -7,3 +7,53 @@ print(mylist)
 # allow duplicate  === items in a list are indexed so they can allow duplicate values
 
 
+thislist = ("apple", "banana", "cherry", "apple", "cherry")
+
+print(thislist)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+
+for i in range(len(thislist)):
+  print(thislist[i])
+
+print(thislist.__len__())
+
+
+# List Comprehension offers the shortest syntax for looping through lists:
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+# sorting list 
+
+thislist = ["orange", "mango", "ANN" ,"kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+
+
+
+
+  
+
+
