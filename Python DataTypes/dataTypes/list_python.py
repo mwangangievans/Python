458c926cd55a8c2f5be132c031fc57d32f6c3ebe
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-02-28 15:15:15 +0300

    lists

---
+++ Python DataTypes/dataTypes/list_python.py
@@ -0,0 +1,9 @@
+# Python Lists
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+# list item are changeble , ordered , 
+
+# allow duplicate  === items in a list are indexed so they can allow duplicate values
+
+
